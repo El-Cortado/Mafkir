718c76dc99780fb3e8729f42a2c3a11ac3cccd15
roy shchory <royshch1@gmail.com>
2020-03-26 19:21:05 +0200

    Reformatting

--- app/src/main/java/com/cortado/mafkir/ui/adapter/DiffCallback.kt
+++ app/src/main/java/com/cortado/mafkir/ui/adapter/DiffCallback.kt
@@ -8,18 +8,14 @@ class DiffCallback(
     private val newList: List<MafkirContact>
 ) : DiffUtil.Callback() {
 
-    // Method #1
     override fun getOldListSize() = oldList.size
 
-    // Method #2
     override fun getNewListSize() = newList.size
 
-    // Method #3
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         return oldList[oldItemPosition].id == newList[newItemPosition].id
     }
 
-    // Method #4
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         return oldList[oldItemPosition].id == newList[newItemPosition].id
                 && oldList[oldItemPosition].contact == newList[newItemPosition].contact
