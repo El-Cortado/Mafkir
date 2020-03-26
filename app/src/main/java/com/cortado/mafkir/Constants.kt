058034fa768aa1aa5eff8877a995c1dea745c882
roy shchory <royshch1@gmail.com>
2020-03-26 19:08:11 +0200

    Added asking for contact permissions

--- app/src/main/java/com/cortado/mafkir/Constants.kt
+++ app/src/main/java/com/cortado/mafkir/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val CHANNEL_ID = "Mafkir_Channel"
         const val SILENT_CHANNEL_ID = "Mafkir_Silent_Channel"
         const val CONTACT_PICKER_RESULT = 1001
+        const val PERMISSION_REQUEST_CONTACT = 1002
     }
 }
--- app/src/main/java/com/cortado/mafkir/permissions/MafkirPermissionsValidator.kt
+++ app/src/main/java/com/cortado/mafkir/permissions/MafkirPermissionsValidator.kt
@@ -1,16 +1,43 @@
 package com.cortado.mafkir.permissions
 
+import android.Manifest
 import android.app.Activity
 import android.app.Application
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.provider.Settings
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.cortado.mafkir.Constants.Companion.PERMISSION_REQUEST_CONTACT
 import javax.inject.Inject
 
 class MafkirPermissionsValidator @Inject constructor(private val application: Application) {
 
     fun validate(activity: Activity) {
+        validateContactAccessPermissions(activity)
+        validateNotificationListenerPermissions(activity)
+    }
+
+    private fun validateContactAccessPermissions(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    activity,
+                    Manifest.permission.READ_CONTACTS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.READ_CONTACTS),
+                    PERMISSION_REQUEST_CONTACT
+                );
+            }
+        }
+    }
+
+    private fun validateNotificationListenerPermissions(activity: Activity) {
         val notificationListenerString: String =
             Settings.Secure.getString(application.contentResolver, "enabled_notification_listeners")
         if (!notificationListenerString.contains(application.packageName)) {
@@ -28,7 +55,11 @@ class MafkirPermissionsValidator @Inject constructor(private val application: Ap
                     "Please enable notification for Mafkir.",
                     Toast.LENGTH_LONG
                 ).show()
-                application.startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS").addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                application.startActivity(
+                    Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS").addFlags(
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+                    )
+                )
             }
             .setNeutralButton("Cancel") { _, _ ->
                 Toast.makeText(
